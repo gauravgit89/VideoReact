b9b04f1a06625450451d6c66b39eb2c40f251c08
gitName <a@b.c>
2020-07-14 15:51:23 +0530

    Updates

---
+++ precache-manifest.f14323899a87719ae5089789e27b1c0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea6de79909bb723e4d2bc9054f8bebc",
+    "url": "/VideoReact/index.html"
+  },
+  {
+    "revision": "a5a38f604b946f6f1160",
+    "url": "/VideoReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0b1eb407b629de2b20a",
+    "url": "/VideoReact/static/js/2.0e0f4479.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VideoReact/static/js/2.0e0f4479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a38f604b946f6f1160",
+    "url": "/VideoReact/static/js/main.4bafe962.chunk.js"
+  },
+  {
+    "revision": "9a4dea4f4366e114d39c",
+    "url": "/VideoReact/static/js/runtime-main.5cbe441b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VideoReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
